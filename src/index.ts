8ce4d5d73ab6ac8f0fff6ac2d520f17129efe2c3
oksanaduga <akakiikaramazov@gmail.com>
2023-12-07 17:03:27 +0300

    feat: parse ast

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const str: string = "ТЕСТ";
+import Lexer from "./Lexer";
+import Parser from "./Parser";
 
-console.log(str);
+const code = 
+    `код РАВНО 5 ПЛЮС 9 ПЛЮС ( 4 МИНУС 6 );
+    КОНСОЛЬ код;
+    переменная РАВНО код ПЛЮС 3;
+    КОНСОЛЬ переменная ПЛЮС код МИНУС 6;`
+
+const lexer = new Lexer(code);
+
+lexer.lexAnalysis();
+
+console.log(lexer.tokenList);
+
+const parser = new Parser(lexer.tokenList);
+
+const rootNode = parser.parseCode();
+
+parser.run(rootNode);
